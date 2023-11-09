a0c0319929d1b076d467613253d5c664ff49f618
prodri69 <127267670+prodri69@users.noreply.github.com>
2023-11-08 19:52:08 -0500

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,26 +1,37 @@
-#1/bin/bash
-#PSQLQUERY HERE
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
 
-#If you run ./element.sh, it should output Please provide an element as an argument. and finish running.
-echo -e"\nPlease provide an element as an argument."
+if [ -z "$1" ]; then
+  echo "Please provide an element as an argument."
+else
+  SYMBOL="$1"
+  if [[ ! $SYMBOL =~ ^[0-9]+$ ]]; then
+    if [ ${#SYMBOL} -gt 2 ]; then
+    ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE name = '$SYMBOL'");
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$SYMBOL'");
+    EL_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE name = '$SYMBOL'");
+    ELEMENT_SYMBOL=$(echo "$EL_SYMBOL" | sed 's/^[[:space:](]\+//;s/[[:space:])]$//');
+    ELEMENT_TYPE=$($PSQL "SELECT types.type FROM types JOIN properties ON types.type_id = properties.type_id JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.name = '$SYMBOL';")
+    ELEMENT_MASS=$($PSQL "SELECT atomic_mass FROM properties JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.name = '$SYMBOL'")
+    MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.name = '$SYMBOL'")
+    BOILING_POINT=$($PSQL "SELECT properties.boiling_point_celsius FROM properties  JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.name = '$SYMBOL'")
+    echo "The element with the atomic number"$ATOMIC_NUMBER" is "$ELEMENT_NAME" ($ELEMENT_SYMBOL). It's a"$ELEMENT_TYPE", with a mass of"$ELEMENT_MASS" amu. "$ELEMENT_NAME" has a melting point of"$MELTING_POINT" celsius and a boiling point of"$BOILING_POINT" celsius."
+    else
+    ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE symbol = '$SYMBOL'");
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$SYMBOL'");
+    ELEMENT_TYPE=$($PSQL "SELECT types.type FROM types JOIN properties ON types.type_id = properties.type_id JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.symbol = '$SYMBOL';")
+    ELEMENT_MASS=$($PSQL "SELECT atomic_mass FROM properties JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.symbol = '$SYMBOL'")
+    MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.symbol = '$SYMBOL'")
+    BOILING_POINT=$($PSQL "SELECT properties.boiling_point_celsius FROM properties  JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.symbol = '$SYMBOL'")
+    echo "The element with the atomic number"$ATOMIC_NUMBER" is "$ELEMENT_NAME" ($SYMBOL). It's a"$ELEMENT_TYPE", with a mass of"$ELEMENT_MASS" amu."$ELEMENT_NAME" has a melting point of"$MELTING_POINT" celsius and a boiling point of"$BOILING_POINT" celsius."
+    fi
+   else
+    ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number = '$SYMBOL'");
+    ELEMENT_TYPE=$($PSQL "SELECT types.type FROM types JOIN properties ON types.type_id = properties.type_id JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.atomic_number = '$SYMBOL';")
+    ELEMENT_MASS=$($PSQL "SELECT atomic_mass FROM properties JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.atomic_number= '$SYMBOL'")
+    MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.atomic_number = '$SYMBOL'")
+    BOILING_POINT=$($PSQL "SELECT properties.boiling_point_celsius FROM properties  JOIN elements ON properties.atomic_number = elements.atomic_number WHERE elements.atomic_number = '$SYMBOL'")
+    echo "The element with the atomic number "$SYMBOL" is "$ELEMENT_NAME" ($SYMBOL). It's a "$ELEMENT_TYPE", with a mass of "$ELEMENT_MASS" amu. "$ELEMENT_NAME" has a melting point of "$MELTING_POINT" celsius and a boiling point of"$BOILING_POINT" celsius."
+  fi
 
-#If you run ./element.sh 1, ./element.sh H, or ./element.sh Hydrogen, it should output The element with atomic number 1 is Hydrogen (H).
-#It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
-#If you run ./element.sh script with another element as input, you should get the same output but with information associated with the given element.
-read EL_ARG
-EL_NAME=$($PSQL "SELECT name FROM elements WHERE name = '$EL_ARG' OR atomic_number = '$EL_ARG' OR symbol = '$EL_ARG';")
-
-#If the argument input to your element.sh script doesn't exist as an atomic_number, symbol, or name in the database, the output should be I could not find that element in the database.
-if [[ -z $EL_NAME ]]
- then
-  echo -e "\nI could not find that element in the database."
- else 
-EL_NUM=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$EL_NAME';");
-EL_SYM=$($PSQL "SELECT symbol FROM elements WHERE name = '$EL_NAME';");
-EL_TYPE=$($PSQL "SELECT type FROM types JOIN properties ON types.type_id = properties.type_id WHERE properties.atomic_number = $EL_NUM")
-EL_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $EL_NUM")
-EL_MELT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $EL_NUM");
-EL_BOIL=$($PSQL "SELECT boling_point_celsius FROM properties WHERE atomic_number = $EL_NUM");
-echo -e "\nThe element with atomic number $EL_NUM is $EL_NAME ($EL_SYM). 'It''s' a $EL_TYPE, with a mass of $EL_MASS amu. $EL_NAME has a melting point of $EL_MELT celsius and a boiling point of $EL_BOIL celsius."
 fi
-
